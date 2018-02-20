d373dc5e6464a37c25d783e5fdd2eeb6fe97078b
tomshacham <shachamtom@gmail.com>
2018-02-20 09:17:03 +0000

    nested routes. App taking config.

--- index.ts
+++ index.ts
@@ -1,9 +1,30 @@
-import {getTo} from "http4js/dist/main/core/RoutingHttpHandler";
+import {getTo, RoutingHttpHandler, ResourceRoutingHttpHandler} from "http4js/dist/main/core/RoutingHttpHandler";
 import {Body} from "http4js/dist/main/core/Body";
 import {Response} from "http4js/dist/main/core/Response";
 
-getTo("/", (req) => {
+class App {
+    private port;
+
+    constructor(config: object) {
+        this.port = config["port"];
+    }
+
+    run(resourceRoutingHttpHandler: ResourceRoutingHttpHandler) {
+        resourceRoutingHttpHandler.asServer(this.port).start()
+    }
+}
+
+let routes = getTo("/", (req) => {
     return new Response(200, new Body("Hello, world!"))
-})
-    .asServer(3000)
-    .start();
+}).withRoutes(getTo("/tosh", (req) => {
+    return new Response(400, new Body("Hello, tosh!"))
+}));
+
+function main(){
+    let config = {port: 3000};
+    console.log("Running with config:");
+    console.log(config);
+    new App(config).run(routes);
+}
+
+main();
